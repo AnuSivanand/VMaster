a99cd16474c9d0c328323bf30fd050eaa3eec18b
Manoj <manoj.m@inapp.com>
2022-04-12 14:16:31 +0530

    access tocken change, Table change

--- src/app/components/market-pairs/market-pairs.component.ts
+++ src/app/components/market-pairs/market-pairs.component.ts
@@ -16,16 +16,18 @@ export class MarketPairsComponent implements OnInit {
   public exchangeIds: any;
   public tickerValues: any;
   public dialogConfig: MatDialogConfig;
+  public favouriteFutureStocks:any;
 
   constructor(    
     private dialog: MatDialog,
   ) { 
     this.dialogConfig = new MatDialogConfig();
+    this.favouriteFutureStocks = [{"id":408065,"stockName":"INFY"}, {"id":884737,"stockName":"TATA Motors"}, {"id":2953217,"stockName":"TCS"}, {"id":341249,"stockName":"HDFCBANK"}];
   }
 
   ngOnInit(): void {
     // this.exchangeIds = [738561];
-    this.exchangeIds = [738561, 408065, 884737, 2953217, 341249];
+    this.exchangeIds = [408065, 884737, 2953217, 341249];
     this.getTickerValues(this.exchangeIds);
   }
 
@@ -36,11 +38,11 @@ export class MarketPairsComponent implements OnInit {
     });
     ticker.on('ticks', (ticks: TickFull) => {
       this.tickerValues = ticks;
-      console.log('tickerValues', this.tickerValues);
     });
 
     ticker.on('connect', () => {
       ticker.subscribe(items);
+      ticker.setMode(ticker.modeFull, items);
     });
     ticker.connect();
   }
@@ -60,4 +62,13 @@ export class MarketPairsComponent implements OnInit {
     const dialogRef = this.dialog.open(BuySellTradeComponent, this.dialogConfig);
     dialogRef.afterClosed().subscribe((data) => { });
   }
+	
+  stockName(type: any){
+	if(type == '341249') { return 'HDFCBANK'; }
+	else if(type == '2953217') { return 'TCS'; }
+	else if(type == '884737') { return 'TATA Motors'; }
+	else if(type == '408065') { return 'INFY'; }
+	else { return "No Name"; }
+  }
+
 }
--- src/app/shared/common/constant.ts
+++ src/app/shared/common/constant.ts
@@ -1,4 +1,4 @@
 export const Constants = {
 	API_KEY: 'bxgemb4liqbi58ki',
-	ACCESS_TOKEN: 'ke5c0F69HP61sIQOhw2z0zKDUDdIQ0MD'
+	ACCESS_TOKEN: 'nMNR7bbPGriFniOA1Y1g16PUrASaTaag'
 };
