269c4226b58046a20ab237b8216d3451614a9161
Manoj <manoj.m@inapp.com>
2022-07-07 18:19:15 +0530

    UI changes

--- src/app/pages/trades/trades.component.ts
+++ src/app/pages/trades/trades.component.ts
@@ -3,6 +3,7 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { ConfirmPopupComponent } from 'src/app/components/confirm-popup/confirm-popup.component';
 import { ApiService } from 'src/app/shared/services/api/api.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-trades',
@@ -22,6 +23,11 @@ export class TradesComponent implements OnInit {
   ) {
     this.dialogConfig = new MatDialogConfig();
     this.getTrades(this.tradeStatus);
+    interval(5000).subscribe(() => {
+    
+      //this.getTrades(this.tradeStatus);
+
+    })
   }
 
   ngOnInit(): void { }
